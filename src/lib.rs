f79ba37d4468f3110f952a3d5673581408e275ce
formula-zero <formula0zero@gmail.com>
2021-07-29 23:43:00 +0900

    update_proof, update_commit, verify_upk

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
-use std::io::Error; // temp. r1cs::SynthesisError
-use std::{ops::{Div, MulAssign}, usize};
+use std::io::Error; use std::ops::{AddAssign, SubAssign};
+use std::u32;
+// temp. r1cs::SynthesisError
+use std::{ops::{Div, MulAssign, Add}, usize};
 
 use ark_ec::msm::VariableBaseMSM;
 use ark_ec::{AffineCurve, PairingEngine, ProjectiveCurve, msm::FixedBaseMSM};
-use ark_ff::{Field, One, PrimeField, UniformRand};
-use ark_std::rand::Rng;   // in ver3.0 of ark_ec, use ark_std instead of rand::Rng
-use ark_poly::{EvaluationDomain, GeneralEvaluationDomain};
+use ark_ff::{Field, One, Zero, PrimeField, UniformRand};
+use ark_poly::univariate::{DenseOrSparsePolynomial, DensePolynomial};
 
-use core::ops::{Sub};
+use ark_std::rand::Rng;   // in ver3.0 of ark_ec, use ark_std instead of rand::Rng
+use ark_poly::{EvaluationDomain, GeneralEvaluationDomain, Polynomial, UVPolynomial};
+
+use core::ops::{Sub, Mul, Neg};
+
+
+
 
 // Module explanation
 // - ark_poly::GeneralEvaluationDomain
@@ -45,6 +52,10 @@ pub struct Parameters<E: PairingEngine> {
 #[derive(Clone, Eq, PartialEq)]
 pub struct Commitment<E: PairingEngine> {
   pub commit: E::G1Affine,
+}
+
+pub struct Proof<E: PairingEngine> {
+  pub w: E::G1Affine,
 }
 
 fn group_gen<E: PairingEngine> (domain: &GeneralEvaluationDomain<E::Fr>) -> E::Fr {
@@ -171,4 +182,223 @@ pub fn commit<E: PairingEngine>(
   };
   
   Ok(c)
+}
+
+pub fn prove_pos<E: PairingEngine>(
+  prk_params: &ProvingKey<E>,
+  values: Vec<E::Fr>,
+  points: Vec<u32>,
+) -> Result<Proof<E>, Error> {
+  let mut values = values.clone();
+  let domain: GeneralEvaluationDomain<E::Fr> =
+    EvaluationDomain::<E::Fr>::new(prk_params.list_g1_tau_i.len()-1)
+      .ok_or(Error)?;
+  domain.ifft_in_place(&mut values);
+
+  // compile error without use 'UVPolynomial' - Univariate
+  // from_coefficients_vec : Constructs a new polynomial from a list of coefficients.
+  // phi(x)
+  let polynomial = DensePolynomial::from_coefficients_vec(values);
+
+  // ∏(x-ω^i)
+  let mut divisor_polynomial = 
+    DensePolynomial::from_coefficients_vec(vec![E::Fr::one()]);
+  for point in points.iter() {
+    let tpoly = DensePolynomial::from_coefficients_vec(vec![
+      group_gen::<E>(&domain).pow(&[*point as u64]).neg(),  // compile error without core::ops::Neg
+      E::Fr::one(),
+    ]); 
+    divisor_polynomial = divisor_polynomial.mul(&tpoly);  // compile eror without core::ops::Mul
+  };
+
+  // Φ(x) / A_I(x) = q(x) ... r(x)  (quotient / remainder)
+  let dense_or_sparse_poly: DenseOrSparsePolynomial<'_, E::Fr> = polynomial.into();
+  let dense_or_sparse_divisor : DenseOrSparsePolynomial<'_, E::Fr> = divisor_polynomial.into();
+  // Divide self by another (sparse or dense) polynomial, and returns the quotient and remainder.
+  let (witness_polynomial, _) = dense_or_sparse_poly
+    .divide_with_q_and_r(&dense_or_sparse_divisor)  
+    .unwrap();
+  
+  // π = g_1^q(τ)
+  // ??? where tau came from and how to apply ??? <-- Already calucated with list_g1_tau_i
+  let scalars: Vec<<E::Fr as PrimeField>::BigInt> = 
+    witness_polynomial.iter().map(|v| v.into_repr()).collect();
+  let witness = VariableBaseMSM::multi_scalar_mul(&prk_params.list_g1_tau_i, &scalars);
+  
+  let proof = Proof::<E> {
+    w: witness.into_affine(),
+  };
+
+  Ok(proof)
+}
+
+// VC.VerifyPos(vrk, c, vI , I, πI )
+pub fn verify_pos<E: PairingEngine> (
+  vrk_params: &VerifyingKey<E>,
+  commit: &Commitment<E>,
+  point_values: Vec<E::Fr>,
+  points: Vec<u32>,
+  proof: &Proof<E>,
+  omega: E::Fr,         // ?? domain??
+) -> Result<bool, Error> {
+
+  // A_I(x) = ∏(x - ω^i)
+  let mut a_polynomial = DensePolynomial::from_coefficients_vec(vec![E::Fr::one()]);
+  for point in points.iter() {
+    let tpoly = DensePolynomial::from_coefficients_vec(vec![
+      omega.pow(&[*point as u64]).neg(),
+      E::Fr::one(), // x - first 
+    ]);
+    a_polynomial = a_polynomial.mul(&tpoly);
+  };
+
+  // r(x) = ∑（l_i * v_i） = ∑（A_I(x) * v_i）/(A_I'(ω^i)(x - ω_i))
+  let mut r_polynomial = DensePolynomial::from_coefficients_vec(vec![E::Fr::zero()]);
+  for (point, value) in points.iter().zip(point_values.iter()) {
+    // x - ω_i
+    let tpoly = DensePolynomial::from_coefficients_vec(vec![
+      omega.pow(&[*point as u64]).neg(),
+      E::Fr::one(),
+    ]);
+    // A_I(x)/(x - ω_i)
+    let mut l_polynomial = a_polynomial.div(&tpoly);
+    // A_I'(ω^i)
+    let b_aside = l_polynomial.evaluate(&omega.pow(&[*point as u64]));
+
+    // v_i/A_I'(ω^i)
+    let bpoly = DensePolynomial::from_coefficients_vec(vec![value.div(&b_aside)]);
+
+    // (A_I(x) /(x - ω_i)) * (v_i/(A_I'(ω^i))
+    l_polynomial = l_polynomial.mul(&bpoly);
+    
+    r_polynomial = r_polynomial.add(l_polynomial);  // compile error without core::ops::Add
+  };
+
+  // Returns the underlying representation of the prime field element.
+  // fn into_repr(&self) -> Self::BigInt
+  let scalars: Vec<<E::Fr as PrimeField>::BigInt> = 
+    r_polynomial.iter().map(|v| v.into_repr()).collect(); 
+  
+  //g^RI (τ)
+  let r_value = VariableBaseMSM::multi_scalar_mul(&vrk_params.list_g1_tau_i, &scalars);
+
+  // e(c/g^R_I(τ), g) = e(π_I , g^A_I(τ)).
+  let mut inner = commit.commit.into_projective();
+  inner.sub_assign(&r_value); // x -= 1 // G1Projective - why sub , not div?? 
+  let lhs = E::pairing(inner, vrk_params.list_g2_tau_i[0]);
+
+  // A_I(τ) = ∏(τ - ω^i)  --> // A_I(x) = ∏(x - ω^i)
+  let a_scalars: Vec<<E::Fr as PrimeField>::BigInt> =
+    a_polynomial.iter().map(|v| v.into_repr()).collect();
+  let a_value = VariableBaseMSM::multi_scalar_mul(&vrk_params.list_g2_tau_i, &a_scalars);
+
+  let rhs = E::pairing(proof.w, a_value);
+
+  Ok(lhs == rhs)
+
+}
+
+//(vrk, i, upk_i)
+pub fn verify_upk<E: PairingEngine> (
+  vrk_params: &VerifyingKey<E>,
+  point: u32,
+  upk: &UpdateKey<E>,
+  omega: E::Fr,
+) -> Result<bool, Eror> {
+  // e(a_i, g^i/g^(w^i)) = e(a,g)
+    // to prove that w^i is a root of X^n -1
+  let omega_i = omega.pow(&[point as u64]);
+
+  // g^i/g^(w^i)
+  let inner = vrk_params.list_g2_tau_i[1].into_projective().sub(
+    &vrk_params.list_g2_tau_i[0].into_projective().mul(omega_i.into()),
+  );
+  let lhs = E::pairing(upk.a_i, inner);
+
+  let rhs = E::pairing(vrk_params.a,
+                                  vrk_params.list_g2_tau_i[0]);
+  let rs1 = lhs == rhs;
+
+  //e(l_i/g1, g) = e(u_i  , g^τ /g(ω_i))
+  //a_i^(1/A'(ω^i))
+  let n = vrk_params.list_g1_tau_i.len() - 1;
+  let a_aside_omega_i_divisor = omega.pow(&[point as u64])
+                          .div(&E::Fr::from_repr((n as u64).into()).unwrap());  // ??????
+  let l_value = upk.a_i.mul(a_aside_omega_i_divisor);
+
+  let inner2 = l_value.sub(&vrk_params.list_g1_tau_i[0].into_projective());
+  let lhs = E::pairing(inner2, vrk_params.list_g2_tau_i[0]);
+
+  let rhs = E::pairing(upk.u_i, inner);
+  let rs2 = lhs == rhs;
+
+  Ok(rs1 && rs2)
+}
+
+//(c, δ, j, upk_j)
+// c' = c·(l_j )^δ, where l_j = a_j^(1/A'(ω^j))
+pub fn update_commit<E: PairingEngine> (
+  commit: &Commitment<E>,
+  delta: E::Fr,
+  point: u32,
+  upk: &UpdateKey<E>,
+  omega: E::Fr,
+  n: usize, // ??
+) -> Result<Commitment<E>, Error> {
+
+  let a_aside_omega_i_divisor = omega.pow(&[point as u64])
+      .div(&E::Fr::from_repr((n as u64).into()).unwrap());  //??
+  let l_value = upk.a_i.mul(a_aside_omega_i_divisor);
+
+  let new_commit = commit.commit.into_projective().add(&(l_value.mul(delta.into())));
+
+  let c = Commitment::<E>{
+    commit: new_commit.into_affine(),
+  };
+
+  Ok(c)
+
+}
+
+
+pub fn update_proof<E: PairingEngine> (
+  proof: Proof<E>,
+  delta: E::Fr,
+  point_i: u32,
+  point_j: u32,
+  upk_i: &UpdateKey<E>,
+  upk_j: &UpdateKey<E>,
+  omega: E::Fr,
+  n: usize,
+) -> Result<Proof<E>, Error> {
+  let mut new_witness = proof.w.into_projective();
+
+  if point_i == point_j {
+    new_witness.add_assign(&upk_i.u_i.mul(delta));
+  } else { // i =/= j
+    //c_1 = 1/(ω_j - ω_i), c_2 = 1/(ω_i - ω_j)
+    let omega_i = omega.pow(&[point_i as u64]);
+    let omega_j = omega.pow(&[point_j as u64]);
+
+    let c_1 = E::Fr::one().div(&(omega_j.sub(&omega_i)));
+    let c_2 = E::Fr::one().div(&(omega_i.sub(&omega_j)));
+
+    // w_ij = a_j^c_1 * a_i^c2
+    let w_ij = upk_j.a_i.mul(c_1).add(&upk_i.a_i.mul(c_2));
+
+    // u_ij = w_ij ^ (1/A'(w^j))
+    let a_aside_omega_i_divisor = omega
+      .pow(&[point_j as u64])
+      .div(&E::Fr::from_repr((n as u64).into()).unwrap());  // why n ???
+    let u_ij = w_ij.mul(a_aside_omega_i_divisor.into());
+    new_witness.add_assign(&u_ij.mul(delta.into()));
+    
+  };
+
+  let proof = Proof::<E> {
+    w: new_witness.into_affine(),
+  };
+
+  Ok(proof)
+
 }
